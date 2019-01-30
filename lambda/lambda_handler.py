da41fa2cda2bcd662537ad4b1a80f1343a24404e
Romain Body <42066902+rbody@users.noreply.github.com>
2019-01-30 08:31:04 +0100

    Feature/merge lambda (#18)
    * merge lambda
    
    * merge lambda
    
    * revert circleci config

--- lambda/lambda_handler.py
+++ lambda/lambda_handler.py
@@ -9,24 +9,21 @@ def handler(event, context):
     except KeyError:
         environment = "dev"
 
-    token = event['token']
-    user_id = event['user_id']
-    command = event['command']
-    try:
-        text = event['text']
-    except:
-        text = ""   # Slash command has no text and is pretty straight forward
-    response_url = event['response_url']
+    if 'event' in event:
+        # This is a Slack event
+        certibot = CertibotEvents(environment)
+        return certibot.launch(event)
 
-    certibot = CertibotCommands(environment, token, user_id, command, text, response_url)
-    certibot.launch()
+    elif 'token' in event:
+        # This is a Slack slash command
+        token = event['token']
+        user_id = event['user_id']
+        command = event['command']
+        try:
+            text = event['text']
+        except:
+            text = ""   # Slash command has no text and is pretty straight forward
+        response_url = event['response_url']
 
-def event_handler(event, context):
-    """This method is the start point for slack events."""
-    try:
-        environment = os.environ['ENV']
-    except KeyError:
-        environment = "dev"
-
-    certibot = CertibotEvents(environment)
-    return certibot.launch(event)
+        certibot = CertibotCommands(environment, token, user_id, command, text, response_url)
+        certibot.launch()
